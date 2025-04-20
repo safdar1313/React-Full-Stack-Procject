f8e805d5f2b6a6e002ee84162836ec4a80e0ac4a
SafdarAli1313 <safdarahmad003@gmail.com>
2025-04-20 12:02:12 +0500

    Update Error404.jsx

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import './pagesStyle.css'
-import Breadcrumb from '../dynamic-components/Breadcrumb';
 function Error404() {
   return (
     <>
-      <Breadcrumb title={"Page Not Found!"}/>
       <div className='w-full error404 flex items-center '>
         <div className='container tex-center mt-[60px]'>
             <h1 className='text-center text-5xl'>Not Found 404!</h1>
